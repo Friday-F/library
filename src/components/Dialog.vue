c01c204ea27a9750cec3d29b72529095c2e594a9
crayon <2545497785@qq.com>
2020-07-14 00:14:37 +0800

    feat:注册

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -10,7 +10,7 @@
             </div>
             <div class="dialog-footer">
                 <a href="javascript:;" class="cancel" @click="cancelBtn">取消</a>
-                <a href="javascript:;" class="submit" @click="submitBtn">登录</a>
+                <a href="javascript:;" class="submit" @click="submitBtn">注册</a>
             </div>
         </div>
     </div>
